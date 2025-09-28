b6aa5726d60ed3533de9eae586eb792cbfbdeaf8
Adepitan <akoredeadetunji93@gmail.com>
2025-09-28 16:15:26 +0100

    Render Error

--- build.sh
+++ build.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 set -o errexit
 
-echo "Installing dependencies..."
 pip install -r requirements.txt
 
-echo "Running migrations..."
-python manage.py migrate --run-syncdb
+# Force migrations
+python manage.py migrate --verbosity=2
+python manage.py migrate --run-syncdb --verbosity=2
 
-echo "Collecting static files..."
 python manage.py collectstatic --clear --noinput
