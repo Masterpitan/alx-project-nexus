e01452ef0b9d7181d76fb356f83cd8812dd891ac
Adepitan <akoredeadetunji93@gmail.com>
2025-09-28 15:32:44 +0100

    Feat: Database Error

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 set -o errexit
 
 pip install -r requirements.txt
+python manage.py makemigrations
+python manage.py migrate
 python manage.py collectstatic --clear --noinput --verbosity 2
-python manage.py migrate --verbosity 2
